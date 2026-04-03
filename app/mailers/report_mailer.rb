6fbfcf73820b51a5991ec69cd379afc004bd4cf3
Bandana <Bandana@Vikrams-MacBook-Air.local>
2026-04-03 11:10:03 +0530

    Added AI summary while sending candidates report to email

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -4,8 +4,17 @@ class ReportMailer < ApplicationMailer
   #########################################
   # 📄 Send Top Candidates Report
   #########################################
-  def shortlist_email(email, candidates, job_description)
+  def shortlist_email(email, candidates, job_description, provider = nil)
     @job_description = job_description
+
+    #########################################
+    # 🔥 Generate AI Summary
+    #########################################
+    @ai_summary = CandidateComparisonService.new(
+        candidates: candidates,
+        job_description: job_description,
+        provider: provider
+    ).call
 
     pdf = CandidateReportPdfService
             .new(candidates: candidates, job_description: job_description, title: "Top Candidates Report")
